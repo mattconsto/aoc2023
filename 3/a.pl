f139d1396bb577ed55b7b9225900f73492bb57e5
Matthew Consterdine <mrconsterdine@googlemail.com>
2023-12-03 11:58:58 +0000

    Add day 3

---
+++ 3/a.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::Util qw(max min);
+
+my @lines;
+while (<STDIN>) {
+    chomp;
+    push @lines, [split //, $_];
+}
+
+my $sum = 0;
+sub check_part {
+    my ($start_y, $start_x, $end_x) = @_;
+
+    for (my $y = max(0, $start_y - 1); $y <= min(scalar @lines - 1, $start_y + 1); $y++) {
+        for (my $x = max(0, $start_x - 1); $x <= min(scalar @{$lines[$y]} - 1, $end_x + 1); $x++) {
+            if ($lines[$y][$x] =~ /[^\d\.]/) {
+                $sum += join '', @{$lines[$start_y]}[$start_x..$end_x];
+                return;
+            }
+        }
+    }
+}
+
+for (my $y = 0; $y < scalar @lines; $y++) {
+    my $start_x;
+
+    my $x = 0;
+    for (; $x < scalar @{$lines[$y]}; $x++) {
+        if (defined $start_x) {
+            if ($lines[$y][$x] !~ /\d/) {
+                check_part($y, $start_x, $x - 1);
+                $start_x = undef;
+            }
+        } elsif ($lines[$y][$x] =~ /\d/) {
+            $start_x = $x;
+        }
+    }
+
+    check_part($y, $start_x, $x - 1) if defined $start_x;
+}
+
+print "$sum\n";
