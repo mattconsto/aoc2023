7ee3731b0af585108c1719e7e9f2b74343d5f568
Matthew Consterdine <mrconsterdine@googlemail.com>
2023-12-07 10:33:13 +0000

    Add day 6

---
+++ 6/a.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::MoreUtils qw(pairwise);
+use List::Util qw(product);
+use POSIX qw(ceil floor);
+
+my $eps = 1e-2;
+
+my @times;
+my @distances;
+while (<STDIN>) {
+    chomp;
+    @times = split /\s+/, $1 if /^\s*Time\s*:\s*(.*)/;
+    @distances = split /\s+/, $1 if /^\s*Distance\s*:\s*(.*)/;
+}
+
+print product(pairwise {floor(($a + sqrt($a**2 - 4*$b)) / 2 - $eps) - ceil(($a - sqrt($a**2 - 4*$b)) / 2 + $eps) + 1} @times, @distances) . "\n";
